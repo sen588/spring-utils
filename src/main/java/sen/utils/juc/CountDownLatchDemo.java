2236c4696dfaad1f470c538e22400b2a45982fde
2050531502 <2050531502@qq.com>
2019-07-09 21:26:18 +0800

    灭六国，秦统一。

--- src/main/java/sen/utils/juc/CountDownLatchDemo.java
+++ src/main/java/sen/utils/juc/CountDownLatchDemo.java
@@ -2,14 +2,27 @@ package sen.utils.juc;
 
 import java.util.concurrent.CountDownLatch;
 
+public class CountDownLatchDemo {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountDownLatch downLatch = new CountDownLatch(6);
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 国已灭亡！！！");
+                downLatch.countDown();
+            }, mySQL.forEach_mySQL(i).getUserName()).start();
+        }
+        downLatch.await();
+        System.out.println("秦国一统天下。");
+    }
+}
+
+
 enum mySQL {
 
-    ONE(1,"齐"),
-    TWO(2,"楚"),
-    THREE(3,"燕"),
-    FOUR(4,"赵"),
-    FIVE(5,"魏"),
-    SIX(6,"韩");
+    ONE(1, "齐"), TWO(2, "楚"), THREE(3, "燕"), FOUR(4, "赵"), FIVE(5, "魏"), SIX(6, "韩");
 
     private Integer id;
 
@@ -31,40 +44,18 @@ enum mySQL {
         this.userName = userName;
     }
 
-    mySQL(Integer id, String userName)
-    {
+    mySQL(Integer id, String userName) {
         this.id = id;
         this.userName = userName;
     }
 
-    public static mySQL forEach_mySQL(int index)
-    {
+    public static mySQL forEach_mySQL(int index) {
         mySQL[] sql = mySQL.values();
-        for (mySQL values: sql) {
-            if(index == values.id)
-            {
+        for (mySQL values : sql) {
+            if (index == values.id) {
                 return values;
             }
         }
         return null;
     }
 }
-
-public class CountDownLatchDemo {
-
-
-    public static void main(String[] args) throws InterruptedException {
-
-        CountDownLatch downLatch = new CountDownLatch(6);
-        for(int i = 1; i <= 6; i++)
-        {
-            new Thread(()->{
-                System.out.println(Thread.currentThread().getName() + " 国已灭亡！！！");
-                downLatch.countDown();
-            }, mySQL.forEach_mySQL(i).getUserName()).start();
-        }
-        downLatch.await();
-        System.out.println("秦国一统天下。");
-    }
-}
-
