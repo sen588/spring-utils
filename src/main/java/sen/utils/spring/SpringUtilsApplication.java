570a9de62664b8024990afa9617fb13fb1504a30
2050531502 <2050531502@qq.com>
2019-07-09 21:13:16 +0800

    spring主类

---
+++ src/main/java/sen/utils/spring/SpringUtilsApplication.java
@@ -0,0 +1,13 @@
+package sen.utils.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUtilsApplication.class, args);
+    }
+
+}
