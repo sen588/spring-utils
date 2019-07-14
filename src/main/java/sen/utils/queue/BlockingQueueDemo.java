488920f86b84d82248833f365c2c702c749c06e0
2050531502 <2050531502@qq.com>
2019-07-14 09:40:59 +0800

    BlockingQueueDemo阻塞队列

--- src/main/java/sen/utils/queue/BlockingQueueDemo.java
+++ src/main/java/sen/utils/queue/BlockingQueueDemo.java
@@ -1,9 +1,29 @@
 package sen.utils.queue;
 
-public class BlockingQueueDemo {
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueDemo
+{
+
+    /*
+    队列：先进先出
+
+    队列空时，从队列中获取操作时队列将会被阻塞
+
+    队列满时，从队列中添加操作时队列将会被阻塞
+     */
     public static void main(String[] args)
     {
-
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        System.out.println("往阻塞队列插入值");
+        blockingQueue.add("a");
+        blockingQueue.add("b");
+        blockingQueue.add("c");
+        System.out.println("移除阻塞队列的值");
+        blockingQueue.remove();
+        blockingQueue.remove();
+        blockingQueue.remove();
     }
 }
