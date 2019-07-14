7ed01a4e9439ec66b89f28ddd5107056bbe848c7
2050531502 <2050531502@qq.com>
2019-07-14 23:50:31 +0800

    ThreadPoolExecutor手写线程池

--- src/main/java/sen/utils/queue/MyTreadPoolDemo.java
+++ src/main/java/sen/utils/queue/MyTreadPoolDemo.java
@@ -19,7 +19,7 @@ public class MyTreadPoolDemo {
 
         /*
         拒绝策约：
-            AbortPolicy(默认)：直接抛出RejectedExecutionException异常阻止系统正常运行
+            AbortPolicy(默认)：直接抛出RejectedExecutionException异常阻止系统正常运行了
 
             CallerRunsPolicy：“调用者运行”一种调节机制，该策略既不会抛弃任务，也不会抛出异常，而是将某些任务回退到调用者，从而降低新任务的流量。
 
